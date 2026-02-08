1874d5f38dc6156674c21f4dafd1b5916f36ef63
NIVEzZz <57322009+NIVEzZz@users.noreply.github.com>
2026-02-08 21:40:09 +0200

    Update main.py

--- python_is_trash/lab2/main.py
+++ python_is_trash/lab2/main.py
@@ -1,11 +1,9 @@
-def file_open(file):
+def file(file, x):
     with open (file, "r") as f:
         data = f.readlines()
     data_float = [float(x) for x in data]
-    print (data_float)
-    return data_float
 
-def file_mean_max_min_median(data_float):
+
     max_val = float("-inf")
     min_val = float("inf")
     sum = 0
@@ -26,19 +24,14 @@ def file_mean_max_min_median(data_float):
        data_len //= 2
        data_len -= 1
        median = data_float[data_len]
-
     mean = sum/len(data_float)
 
-    return mean, max_val, min_val, median
 
-def file_write (file):
-    yc = file_open(file)
-    file_mean_max_min_median(yc)
-    f = open(f"out_{file}","w")
-    f.write("Mean:", mean)
-    f.write("Max:", max)
-    f.write("Min:", min)
-    f.write("Median:", median)
+    fa = open(f"out_yc-{x}.dat","w")
+    fa.write(f"Mean: {mean}\n")
+    fa.write(f"Max: {max_val}\n")
+    fa.write(f"Min: {min_val}\n")
+    fa.write(f"Median: {median}\n")
 
 for x in range (1,9 + 1):
-    file_write(f"yc-{x}.dat")
+    file(f"yc-{x}.dat", x)
