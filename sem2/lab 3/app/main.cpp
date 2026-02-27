0181a0e93861caed05ac4f203b9339a024868371
NIVEzZz <57322009+NIVEzZz@users.noreply.github.com>
2026-02-27 12:59:43 +0200

    Create main.cpp

---
+++ sem2/lab 3/app/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include "Product.hpp"
+#include "..\lib\Ingredient.hpp"
+#include <forward_list>
+#include <list>
+
+int main() {
+
+    setlocale(LC_ALL, "Russian");
+
+    try {
+        std::cout << "Тесты" << std::endl;;
+        std::cout << std::endl;
+        mt::Product test1("A", "Описание1", 100, 50, "Адрес1", {"а","б","в"});
+        test1.print();
+        std::cout << std::endl;
+        mt::Product test2("Б", "Описание2", 100, 30, "Адрес2", { "а","м","н" });
+        test2.print();
+        std::cout << std::endl;
+        mt::Product ans1 = test1 + test2;
+        ans1.print();
+        std::cout << std::endl;
+        mt::Product ans2 = test1 - test2;
+        ans2.print();
+        std::cout << std::endl;
+        test1 -= test2;
+        test1.print();
+        std::cout << std::endl;
+        lib::Ingredient a { "хуй",100 };
+        a.print();
+    }
+    catch (const std::exception& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+    }
+
+    return 0;
+}
