0b01a65baa843959fee3ba09614cd8107fb6eb0e
NIVEzZz <57322009+NIVEzZz@users.noreply.github.com>
2026-03-02 00:06:31 +0200

    Update Commission.hpp

--- sem2/lab 3/lib/Commission.hpp
+++ sem2/lab 3/lib/Commission.hpp
@@ -1,29 +1,33 @@
 #pragma once
-#include <string>
-#include <vector>
+#include "..\app\Product.hpp"
 
-using vec = std::vector<std::string>;
+namespace mt {
 
-namespace lib {
-	class Commission {
-	public:
-		// ctor default
-		Commission();
-		// ctor param
-		Commission(int time_to_check, const std::string& address, const vec ban_ingredient);
-		// ctor copy
-		Commission(const Commission& other);
+    class Commission {
+    public:
+        Commission();
+        Commission(const std::string& address,
+            const std::vector<std::string>& forbidden_ingredients,
+            int inspection_days);
+        Commission(const Commission& other);
+        ~Commission();
 
-		// dtor
-		~Commission();
+        void set_address(const std::string& address);
+        std::string get_address() const;
 
-		//------------------------------------------------------------------------------------------------------------------------------------------------------
+        void set_forbidden_ingredients(const std::vector<std::string>& fi);
+        std::vector<std::string> get_forbidden_ingredients() const;
 
-		void Legit_check(int expiration_days, const std::string& name);
+        void set_inspection_days(int days);
+        int get_inspection_days() const;
 
-	private:
-		int time_to_check_;
-		std::string address_;
-		vec ban_ingredient_;
-	};
-}//namespase lib
+        bool check_product(const Product& product) const;
+        void print() const;
+
+    private:
+        std::string address_;
+        std::vector<std::string> forbidden_ingredients_;
+        int inspection_days_;
+    };
+
+} // namespace mt
