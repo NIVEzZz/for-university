95e57ab55ec7b975dab683f8f47f53fc46168a01
NIVEzZz <57322009+NIVEzZz@users.noreply.github.com>
2026-03-01 15:07:01 +0200

    Update main.cpp

--- sem2/lab 3/app/main.cpp
+++ sem2/lab 3/app/main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include "Product.hpp"
-#include "..\lib\Ingredient.hpp"
 #include <forward_list>
 #include <list>
 
@@ -11,22 +10,22 @@ int main() {
     try {
         std::cout << "Тесты" << std::endl;;
         std::cout << std::endl;
-        mt::Product test1("A", "Описание1", 100, 50, "Адрес1", {"а","б","в"});
+        mt::Product test1("A", "Описание1", 100, 50, "Адрес1", { {"а",5.0},{"б",5.0},{"в",5.0}, {"а",5.0} });
         test1.print();
         std::cout << std::endl;
-        mt::Product test2("Б", "Описание2", 100, 30, "Адрес2", { "а","м","н" });
+        mt::Product test2("Б", "Описание2", 100, 30, "Адрес2", { {"а",5.0},{"м",5.0},{"н",5.0} });
         test2.print();
         std::cout << std::endl;
         mt::Product ans1 = test1 + test2;
         ans1.print();
         std::cout << std::endl;
-        mt::Product ans2 = test1 - test2;
-        ans2.print();
+       // mt::Product ans2 = test1 - test2;
+       // ans2.print();
         std::cout << std::endl;
-        test1 -= test2;
+        //test1 -= test2;
         test1.print();
         std::cout << std::endl;
-        lib::Ingredient a { "хуй",100 };
+        lib::Ingredient a { "ааа",100 };
         a.print();
     }
     catch (const std::exception& e) {
