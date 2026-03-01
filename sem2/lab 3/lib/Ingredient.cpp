95286e3736c0a84a9d91ab3210b0c20792eb22f6
NIVEzZz <57322009+NIVEzZz@users.noreply.github.com>
2026-03-02 00:08:43 +0200

    Update Ingredient.cpp

--- sem2/lab 3/lib/Ingredient.cpp
+++ sem2/lab 3/lib/Ingredient.cpp
@@ -1,15 +1,1 @@
-#include "Ingredient.hpp"
-#include <iostream>
 
-namespace lib {
-	void Ingredient::print() const {
-		std::cout << "(" << name_ << ", " << volume_ << ")" << std::endl;
-	}
-	bool Ingredient::operator==(Ingredient& other) {
-		if ((this->name_ == other.name_) and (std::abs(this->volume_ - other.volume_) < std::numeric_limits<float>::lowest())) {
-			return true;
-		}
-		return false;
-	}
-
-}//namespase lib
